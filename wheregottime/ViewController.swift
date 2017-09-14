631a3b76e40174d05679ed91834febfbddb42f2c
qiuhui <qh@Ongs-MacBook-Pro-4.local>
2017-09-14 20:09:30 +0800

    Initial Commit

---
+++ wheregottime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wheregottime
+//
+//  Created by qiuhui on 14/9/17.
+//  Copyright © 2017 qiuhui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
